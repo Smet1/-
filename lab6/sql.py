4c63fb07b66e4f5f2fd72a69c83e7c774c0772b2
Smet1 <32566683+Smet1@users.noreply.github.com>
2017-12-01 17:58:09 +0300

    added a review

---
+++ lab6/sql.py
@@ -0,0 +1,24 @@
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost",
+    user="dbuser",
+    passwd="123",
+    db="first_db",
+    charset="utf8"
+)
+
+c = db.cursor()
+
+# c.execute("INSERT INTO banks (name, address) VALUES (%s, %s);", ('Название', 'адрес'))
+# db.commit()
+
+c.execute("SELECT *FROM banks;")
+
+entries = c.fetchall()
+
+for e in entries:
+    print(e)
+
+c.close()
+db.close()
